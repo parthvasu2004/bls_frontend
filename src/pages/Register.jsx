10d7622a8f2c329d89670f3a5c1f20405366103a
Parth Pandey <119426977+parthvasu2004@users.noreply.github.com>
2026-03-29 15:56:36 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -36,7 +36,7 @@ const Register = () => {
     setIsLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/auth/register", {
+      const res = await fetch("https://bls-backend.vercel.app//auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
